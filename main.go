d677620c6ccdbe723807843723f19504e547b249
glitter <2201409588@qq.com>
2022-04-15 20:48:09 +0800

    cc

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	bx "trader/bexchange"
 )
 
@@ -17,11 +18,20 @@ func main() {
 	//ob.AddOrder(bx.NewOrder(4, true, 55, 75))
 	//ob.AddOrder(bx.NewOrder(5, false, 50, 50))
 	//ob.CancelOrder(1, 50, false)
+	go func() {
+		for {
+			time.Sleep(30 * time.Second)
+			ob.BuyPrice5()
+			ob.SellPrice5()
+			ob.GetFinalPrice()
+		}
+	}()
 	ob.AddOrder(bx.NewOrder(1, true, 2082.34, 1))
-	ob.AddOrder(bx.NewOrder(2, false, 2087.6, 2))
+	ob.AddOrder(bx.NewOrder(2, true, 2087.6, 2))
 	ob.AddOrder(bx.NewOrder(3, true, 2087.8, 1))
 	ob.AddOrder(bx.NewOrder(4, true, 2085.01, 5))
-	ob.AddOrder(bx.NewOrder(5, false, 2088.02, 3))
+	ob.AddOrder(bx.NewOrder(5, true, 2088.02, 3))
+
 	ob.AddOrder(bx.NewOrder(6, false, 2087.60, 6))
 	ob.AddOrder(bx.NewOrder(7, true, 2081.77, 7))
 	ob.AddOrder(bx.NewOrder(8, true, 2086.0, 3))
